76ddf9d4cc28db2ccfbd4a62f069fd55037e505b
ufukgulec <oug505@gmail.com>
2021-04-01 00:07:05 +0300

    Home Index Layout düzen

--- Kurs.MvcUI/Controllers/HomeController.cs
+++ Kurs.MvcUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kurs.Business;
+using Kurs.Dal.Concrete.EntityFramework.Repository;
+using Kurs.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,16 @@ namespace Kurs.MvcUI.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        ILessonService lessonService = new LessonManager(new EfLessonRepository());
+        IStudentService studentService = new StudentManager(new EfStudentRepository());
+        ITeacherService teacherService = new TeacherManager(new EfTeacherRepository());
         public ActionResult Index()
         {
+            ViewBag.StudentCount = studentService.GetAll().Count;
+            ViewBag.TeacherCount = teacherService.GetAll().Count;
+            ViewBag.LessonCount = lessonService.GetAll().Count;
+
+            ViewBag.LastStudent = studentService.GetAll().OrderByDescending(s => s.kayitTarihi).FirstOrDefault().ogrenciAdi;
             return View();
         }
     }
